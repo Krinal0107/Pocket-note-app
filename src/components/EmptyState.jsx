0909b89c96e6cee02af5de944bad8ca6ffd291d0
Krinal <142133673+Krinal0107@users.noreply.github.com>
2025-08-28 08:50:51 +0530

    commited

---
+++ src/components/EmptyState.jsx
@@ -0,0 +1,15 @@
+import './EmptyState.css'
+import Illustration from './Illustration.jsx'
+
+export default function EmptyState() {
+  return (
+    <main className="content">
+      <div className="center-wrap">
+        <Illustration className="illustration" />
+        <h1 className="headline">Pocket Notes</h1>
+        <p className="lead">Send and receive messages without keeping your phone online.</p>
+        <p className="lead">Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
+      </div>
+    </main>
+  )
+}
